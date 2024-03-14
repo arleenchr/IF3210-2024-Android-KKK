9c12a85eebd65702bf739d4c77a5e237748005aa
maikeljh <mjh191003@gmail.com>
2024-03-14 10:13:00 +0700

    [New Feature] Connect splash screen with android manifest

--- app/src/main/java/com/example/bondoman/SplashActivity.kt
+++ app/src/main/java/com/example/bondoman/SplashActivity.kt
@@ -5,18 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.WindowManager
 
-class SplashScreen : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-//        window.setFlags(
-//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//            WindowManager.LayoutParams.FLAG_FULLSCREEN
-//        )
-
+        supportActionBar?.hide()
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
