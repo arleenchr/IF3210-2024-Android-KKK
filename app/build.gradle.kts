ae5d7ebf08721eb1ac9de70e26972bf3fc7ec8e7
Alifioditya <alifioditya@gmail.com>
2024-03-15 18:21:43 +0700

    feat: activity fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-    implementation("com.github.yuriy-budiyev:code-scanner:2.3.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
--- app/src/main/java/com/example/bondoman/ui/pie_chart/PieChartFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/pie_chart/PieChartFragment.kt
@@ -1,60 +1,103 @@
 package com.example.bondoman.ui.pie_chart
 
+import com.example.bondoman.R
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.bondoman.R
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.ColorTemplate
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PieChartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PieChartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class PieChartFragment() : Fragment(), OnChartValueSelectedListener {
+    private var chart: PieChart? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pie_chart, container, false)
+        return inflater.inflate(R.layout.fragment_analytics, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        chart = view.findViewById(R.id.pieChart)
+
+        setupChart()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PieChartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PieChartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setupChart() {
+        chart?.apply {
+            setUsePercentValues(true)
+            description.isEnabled = false
+            setExtraOffsets(5f, 10f, 5f, 5f)
+            setDragDecelerationFrictionCoef(0.95f)
+
+            isDrawHoleEnabled = false
+            rotationAngle = 0f
+            isRotationEnabled = true
+            isHighlightPerTapEnabled = true
+
+            setOnChartValueSelectedListener(this@PieChartFragment)
+
+            animateY(1400, Easing.EaseInOutQuad)
+
+            setEntryLabelColor(Color.WHITE)
+            setEntryLabelTextSize(12f)
+        }
+
+        // Set dummy data
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(18.5f, 0))
+        entries.add(PieEntry(26.7f, 1))
+
+        val dataSet = com.github.mikephil.charting.data.PieDataSet(entries, "Results")
+        dataSet.setDrawIcons(false)
+        dataSet.sliceSpace = 3f
+        dataSet.selectionShift = 5f
+        dataSet.iconsOffset = com.github.mikephil.charting.utils.MPPointF(0f, 40f)
+
+        val colors = ArrayList<Int>()
+        colors.add(Color.rgb(34, 197, 94))
+        colors.add(Color.rgb(249, 115, 22))
+        dataSet.colors = colors
+
+        val data = com.github.mikephil.charting.data.PieData(dataSet)
+        data.setValueFormatter(com.github.mikephil.charting.formatter.PercentFormatter(chart))
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.WHITE)
+
+        chart?.highlightValues(null)
+        chart?.data = data
+        chart?.invalidate()
+
+        // Disable legend
+        val l: Legend = chart!!.legend
+        l.isEnabled = false
+    }
+
+    override fun onValueSelected(e: Entry, h: Highlight) {
+    Log.i(
+            "VAL SELECTED",
+            "Value: " + e.y + ", index: " + h.x
+                    + ", DataSet index: " + h.dataSetIndex
+        )
+    }
+
+    override fun onNothingSelected() {
+        Log.i("PieChart", "nothing selected")
     }
 }
