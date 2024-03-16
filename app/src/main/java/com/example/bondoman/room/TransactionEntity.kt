8e339a1eb8f73776c28a7466ba6ed21d98a5ad27
maikeljh <mjh191003@gmail.com>
2024-03-16 18:20:40 +0700

    [Update] transaction database

--- app/src/main/java/com/example/bondoman/room/TransactionEntity.kt
+++ app/src/main/java/com/example/bondoman/room/TransactionEntity.kt
@@ -3,10 +3,15 @@ package com.example.bondoman.room
 import android.location.Location
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.sql.Timestamp
 
 @Entity(tableName = "transaction", primaryKeys = ["title", "createdAt"])
 data class TransactionEntity (
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        var id: Long = 0,
+
         @ColumnInfo(name = "title")
         var title: String = "",
 
